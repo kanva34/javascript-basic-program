e60903a62924054f9cbab9bdabc6fba19c6fcebf
Talha Bilal <talhabilalkanva180@gmail.com>
2025-08-17 08:28:37 -0700

    kids name collector
    // This program asks the user how many children they have, validates the input, collects each child's name, and then displays all names together.

---
+++ kids-name-collector/script.js
@@ -0,0 +1,18 @@
+//* // This program asks the user how many children they have, validates the input, collects each child's name, and then displays all names together.
+
+// let numofchildren = prompt("How many children do you have");
+
+// while (isNaN(numofchildren) || numofchildren.trim() === "" || numofchildren <= 0) {
+//   numofchildren = prompt("Please enter a valide number of children");
+// }
+// numofchildren = Number(numofchildren);
+// let childrenNames = [];
+// let i = 1;
+
+// while (i <= numofchildren) {
+//   let Names = prompt(`What is the name of the children ${i} ?`);
+//   childrenNames.push(Names);
+//   i++;
+// }
+// alert(`Your childern names are ${childrenNames.join(", ")}`);
+// console.log(`Your children names are ${childrenNames}`);
