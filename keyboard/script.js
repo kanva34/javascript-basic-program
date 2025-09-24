98d35c92612195f43d77c46a2edf8bf758a4a71e
Talha Bilal <talhabilalkanva180@gmail.com>
2025-09-24 00:10:33 -0700

    Keyboard Keys

---
+++ keyboard/script.js
@@ -0,0 +1,20 @@
+const insert = document.getElementById("insert");
+
+window.addEventListener("keydown", (e) => {
+  insert.innerHTML = `
+    <div class="key">
+      <table>
+        <tr>
+          <th>Key</th>
+          <th>Code</th>
+          <th>Key Code</th>
+        </tr>
+        <tr>
+          <td>${e.key === " " ? "Space" : e.key}</td>
+          <td>${e.code}</td>
+          <td>${e.keyCode}</td>
+        </tr>
+      </table>
+    </div>
+  `;
+});
